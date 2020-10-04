6f13fe148d758c7641b8d0204863b6a6ae5e1860
claudine Kuradusenge <claudinekuradusenge2@gmail.com>
2020-10-04 12:10:21 +0200

    add projects files

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+$('.icon1').click(function() {
+    $('#icon1').show();
+    $(".icon1").hide();
+
+});
+$('#icon1').click(function() {
+    $('.icon1').show();
+    $("#icon1").hide();
+
+});
+$('.icon2').click(function() {
+    $('#icon2').show();
+    $(".icon2").hide();
+
+});
+$('#icon2').click(function() {
+    $('.icon2').show();
+    $("#icon2").hide();
+
+});
+
+$('.icon3').click(function() {
+    $('#icon3').show();
+    $(".icon3").hide();
+
+});
+$('#icon3').click(function() {
+    $('.icon3').show();
+    $("#icon3").hide();
+
+});
+
+$("form").submit(function(event) {
+    if ($("input").val() === "") {
+        alert('Enter valid data')
+        return;
+    }
+
+    alert('Your message have been recevied')
+    event.preventDefault();
+    this.reset();
+});
